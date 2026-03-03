76ca0f0a9a244a051ebd425f531482f1d4388405
Carlos Torres <carlos30847012@gmail.com>
2026-03-03 12:06:07 -0400

    feat: add initial configuration

---
+++ backend/src/server.ts
@@ -0,0 +1,23 @@
+import express from "express";
+import db from "./config/database";
+
+const connectDB = async () => {
+    try {
+        await db.authenticate();
+        console.log("Database connected");
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.json("OK!")
+})
+
+export default app;
